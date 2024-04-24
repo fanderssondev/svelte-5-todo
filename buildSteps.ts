d22d2c66ece66aaea9da8cdf4514d70e2a158df0
Fredrik <fredrikanderssondev@gmail.com>
2024-04-24 20:58:23 +0200

    Initial project setup

---
+++ buildSteps.ts
@@ -0,0 +1,27 @@
+// TODO: Style
+// [X]: CSS reset
+// []: Darkmode
+// []: Icons for todos, possibly changeable
+
+// TODO: Structure
+// []: Create input form
+// []: Define Todo type
+// []: List todos
+// []: Create buttons for all, unfinished and finished todos
+// []: Refactor to store
+// []: Refactor to components
+
+// TODO: Funcionality
+// []: Local storage
+// []: Colors for categories
+// []: Duedate for todos
+// []: Login
+// []: Database
+
+// TODO: Page header
+// []: Logo
+// []: Login button
+// []: Sign out button
+// []: Toggle darkmode button
+
+// TODO: Start page
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<h1>Heelo</h1>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
