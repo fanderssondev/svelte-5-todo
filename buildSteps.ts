3886a12b821e5a6a77d12ada5f78425af1d270e1
Fredrik Andersson <fredrikanderssondev@gmail.com>
2024-05-08 21:42:42 +0200

    Fixed buildsteps

--- buildSteps.ts
+++ buildSteps.ts
@@ -1,33 +1,33 @@
-// TODO: Style
-// [X]: CSS reset
-// [X]: Darkmode
-// []: Icons for todos, possibly changeable
+// SECTION Style
+// [x] CSS reset
+// [x] Darkmode
+// [ ] Icons for todos, possibly changeable
 
-// TODO: Structure
-// [X]: Create input form
-// [X]: Define Todo type
-// [X]: List todos
-// [X]: Create buttons for all, unfinished and finished todos
-// []: Refactor to components
+// SECTION Structure
+// [x] Create input form
+// [x] Define Todo type
+// [x] List todos
+// [x] Create buttons for all, unfinished and finished todos
+// [ ] Refactor to components
 
-// TODO: Funcionality
-// [X]: Local storage
-// [X]: Add todo
-// [X]: Edit todo
-// [X]: Delete todo
-// [X]: Show number of unfinished todos
-// [X]: Filter todos for all, unfinished and finished todos
-// [X]: Percistance for darkmode
-// []: Colors for categories
-// []: Duedate for todos
-// [X]: Login
-// [X]: Database
+// SECTION Funcionality
+// [x] Local storage
+// [x] Add todo
+// [x] Edit todo
+// [x] Delete todo
+// [x] Show number of unfinished todos
+// [x] Filter todos for all, unfinished and finished todos
+// [x] Percistance for darkmode
+// [ ] Colors for categories
+// [ ] Duedate for todos
+// [x] Login
+// [x] Database
 
-// TODO: Page header
-// [X]: Logo
-// [X]: Login button
-// [X]: Sign up button
-// [X]: Toggle darkmode button
+// SECTION Page todos
+// [ ] Logo
+// [x] Login button
+// [x] Sign up button
+// [x] Toggle darkmode button
 
-// TODO: Start page
-// TODO: Update README
+// TODO Start page
+// TODO Update README
