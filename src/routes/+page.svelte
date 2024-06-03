94c6f9b45ac22543e6608fde5eaeb921f48e67a3
Fredrik <fredrikanderssondev@gmail.com>
2024-06-03 15:38:19 +0200

    Updated starting page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Start page</h1>
+<h1>Todos</h1>
+
+<p>Sign up to create a todo list</p>
+
+<style>
+	h1 {
+		text-align: center;
+		font-size: 5rem;
+		letter-spacing: 4px;
+		margin-bottom: 3rem;
+	}
+
+	p {
+		text-align: center;
+		font-family: Roboto slab;
+		font-size: 1.5rem;
+	}
+</style>
